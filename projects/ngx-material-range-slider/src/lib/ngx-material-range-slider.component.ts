d3f6cb4d4b84bfef661bad2e0dd61f59ef87f94e
Armand-Adrian Colenati <armandcolenati@gmail.com>
2020-10-18 22:01:35 +0300

    Added vertical and horizontal CSS classes

--- projects/ngx-material-range-slider/src/lib/ngx-material-range-slider.component.ts
+++ projects/ngx-material-range-slider/src/lib/ngx-material-range-slider.component.ts
@@ -73,10 +73,10 @@ export class NgxMaterialRangeSliderComponent implements ControlValueAccessor {
   private _highValuePercent: number = 0;
 
   @HostBinding('class') public hostClassName: string = RANGE_SLIDER_CLASS;
-  @HostBinding('class.mat-slider-horizontal]') public isHorizontalSlider(): boolean {
+  @HostBinding('class.mat-range-slider-horizontal]') public isHorizontalSlider(): boolean {
     return !this._vertical;
   }
-  @HostBinding('class.mat-slider-vertical]') public isVerticalSlider(): boolean {
+  @HostBinding('class.mat-range-slider-vertical]') public isVerticalSlider(): boolean {
     return this._vertical;
   }
 
