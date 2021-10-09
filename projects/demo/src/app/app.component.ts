92f1a0f3200b70945fb5e5c7f56a9a8e0dcc777f
Armand-Adrian Colenati <armandcolenati@gmail.com>
2021-10-09 18:41:28 +0300

    Extended demo app with controls

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -8,8 +8,8 @@ const HORIZONTAL_DEFAULT_RANGE: RangeInterval = {
 };
 
 const VERTICAL_DEFAULT_RANGE: RangeInterval = {
-  min: 40,
-  max: 60
+  min: 30,
+  max: 70
 };
 
 @Component({
@@ -19,5 +19,13 @@ const VERTICAL_DEFAULT_RANGE: RangeInterval = {
 })
 export class AppComponent {
   public readonly horizontalSliderControl = new FormControl(HORIZONTAL_DEFAULT_RANGE);
+  public readonly horizontalSliderMinLimitControl = new FormControl(0);
+  public readonly horizontalSliderMaxLimitControl = new FormControl(100);
+  
   public readonly verticalSliderControl = new FormControl(VERTICAL_DEFAULT_RANGE);
+  public readonly verticalSliderMinLimitControl = new FormControl(0);
+  public readonly verticalSliderMaxLimitControl = new FormControl(100);
+  
+  public readonly dynamicSliderControl = new FormControl(HORIZONTAL_DEFAULT_RANGE);
+  public readonly dynamicSliderVerticalControl = new FormControl(false);
 }
